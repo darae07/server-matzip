32465ed5554ad5f55b70512e4f79749f6283d889
darae07 <leedr0730@gmail.com>
2021-06-06 23:08:52 +0900

    create store model

--- stores/admin.py
+++ stores/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Store
+from .models import Dong
+from .models import Menu
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Dong)
+admin.site.register(Menu)
--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
+class Dong(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    dong = models.ForeignKey(Dong, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, default='')
+
+    # lon = models.IntegerField()
+    # lat = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.menu_name
