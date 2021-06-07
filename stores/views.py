05955a77f9f70cfe51edc6c9b60ff95bd61a245f
darae07 <leedr0730@gmail.com>
2021-06-08 00:53:09 +0900

    가게 - 메뉴 리스트 호출 api

--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Store, Dong, Menu, Category
+from django.views import generic
 
 
 # Create your views here.
+class StoreList(generic.ListView):
+    def get_queryset(self):
+        return Store.objects.order_by('id')
+
+
 def index(request):
-    return HttpResponse("Hello, world.")
+    store_list = Store.objects.order_by('id')
+    context = {
+        'stores': store_list,
+    }
+    return HttpResponse(context)
+
+
+class StoreDetail(generic.DetailView):
+    model = Store
