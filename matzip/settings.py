76fefefef50b0f7b2241303d4739ad18a1b2aa81
darae07 <leedr0730@gmail.com>
2021-06-13 19:48:38 +0900

    auth init

--- matzip/settings.py
+++ matzip/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stores.apps.StoresConfig',
     'reviews.apps.ReviewsConfig',
+    'common.apps.CommonConfig',
     'rest_framework'
 ]
 
@@ -158,4 +159,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
