ae62a570a83ff11a8e6208aa57b9818eb853c234
darae07 <leedr0730@gmail.com>
2021-06-16 00:41:18 +0900

    url 정리

--- matzip/urls.py
+++ matzip/urls.py
@@ -17,19 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView
-)
 
 urlpatterns = [
     path('stores/', include('stores.urls')),
     path('reviews/', include('reviews.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('common.urls')),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth', include('knox.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
