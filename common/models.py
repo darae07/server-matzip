9e70761efb62dd2f1cbe12e020d88a23696bd3e6
darae07 <leedr0730@gmail.com>
2021-06-23 23:03:05 +0900

    유저 모델 필드 변경

--- common/models.py
+++ common/models.py
@@ -8,8 +8,10 @@ from django.contrib.auth.models import User, AbstractUser
 
 class CommonUser(AbstractUser):
     nickname = models.CharField(max_length=100, null=True, blank=True)
-    greetings = models.CharField(max_length=400, null=True, blank=True)
-
+    title = models.CharField(max_length=400, null=True, blank=True)
+    image = models.ImageField(upload_to='users', default='', null=True, blank=True)
+    status = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
 
 # @receiver(post_save, sender=User)
 # def create_user_profile(sender, instance, created, **kwargs):
