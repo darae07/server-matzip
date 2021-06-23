8e6dec9f5e8357c992029f6cf2cb6559642a4be8
darae07 <leedr0730@gmail.com>
2021-06-24 00:25:01 +0900

    회사 모델 추가

--- group/admin.py
+++ group/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, Contract, Party, Membership
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Contract)
+admin.site.register(Party)
+admin.site.register(Membership)
--- group/models.py
+++ group/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 from common.models import CommonUser
+from stores.models import Store
 
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    employees = models.ManyToManyField(CommonUser, through='Contract')
+
+    def __str__(self):
+        return self.name
+
+
+class Contract(models.Model):
+    employee = models.ForeignKey(CommonUser, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    team_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.employee.username
+
+
 class Party(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     members = models.ManyToManyField(CommonUser, through='Membership')
+    description = models.CharField(max_length=500, null=True, blank=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
