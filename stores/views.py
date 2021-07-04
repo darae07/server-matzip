6849f828b75f87d6c2cbafea4c97e907f61d08ae
darae07 <leedr0730@gmail.com>
2021-07-04 22:04:48 +0900

    store 검색조건

--- stores/views.py
+++ stores/views.py
@@ -4,6 +4,7 @@ from .models import Store, Menu, Category
 from django.views import generic
 from rest_framework import viewsets
 from .serializer import StoreSerializer, CategorySerializer
+from rest_framework.filters import SearchFilter
 
 
 # Create your views here.
@@ -11,7 +12,11 @@ class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
+    filter_backends = [SearchFilter]
+    search_fields = 'name'
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    
