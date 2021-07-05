54bac793656e8200dd09ca9d22c4504c484acec1
darae07 <leedr0730@gmail.com>
2021-07-05 23:22:14 +0900

    위치 추가

--- stores/models.py
+++ stores/models.py
@@ -14,6 +14,7 @@ class Store(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     lat = models.FloatField(null=True, blank=True)
     lon = models.FloatField(null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
     tel = models.CharField(max_length=50, null=True, blank=True)
     open_time = models.TimeField(null=True, blank=True)
     close_time = models.TimeField(null=True, blank=True)
