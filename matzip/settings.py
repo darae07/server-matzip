2826086be3fdc8917a27870707c1c6787d41eb50
darae07 <leedr0730@gmail.com>
2021-08-03 00:01:58 +0900

    설정 변경

--- matzip/settings.py
+++ matzip/settings.py
@@ -28,10 +28,9 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)qvm(2$7ts_uh5k4xnuny+na76((-9txu)qz5(4ep0yh3f^p='
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
