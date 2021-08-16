3be158637796ae1d7c609e933d91144d51daf675
darae07 <leedr0730@gmail.com>
2021-08-17 01:03:34 +0900

    common user admin 필드셋에 변경된 모델 반영

--- common/admin.py
+++ common/admin.py
@@ -6,7 +6,29 @@ from .models import CommonUser
 
 
 class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'nickname', 'title', 'image', 'status', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    limited_fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'twitter', 'photo')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
+
+    list_display = ('email', 'first_name', 'last_name', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('first_name', 'last_name', 'email')
     ordering = ('email',)
+    readonly_fields = ('last_login', 'date_joined',)
 
 
 # admin.site.unregister(CommonUser)
