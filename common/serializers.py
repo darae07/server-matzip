4e74e110ca96db0b1dfae0c8a5a156b12811d51d
darae07 <leedr0730@gmail.com>
2021-08-22 01:16:08 +0900

    user 이미지 serializer 변경

--- common/serializers.py
+++ common/serializers.py
@@ -8,34 +8,20 @@ from .models import CommonUser
 
 
 class LoginSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-
-    def get_image_url(self, queryset):
-        request = self.context.get('request')
-        if not queryset.image:
-            return None
-        image_url = queryset.image.url
-        return request.build_absolute_uri(image_url)
+    image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = CommonUser
         fields = ['id', 'email', 'last_login', 'is_superuser', 'is_staff',
-                  'date_joined', 'nickname', 'image_url', 'status', 'phone_number']
+                  'date_joined', 'nickname', 'image', 'status', 'phone_number']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-
-    def get_image_url(self, queryset):
-        request = self.context.get('request')
-        if not queryset.image:
-            return None
-        image_url = queryset.image.url
-        return request.build_absolute_uri(image_url)
+    image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = CommonUser
         fields = ['id', 'email', 'last_login',
-                  'date_joined', 'nickname', 'image_url', 'status', 'phone_number']
+                  'date_joined', 'nickname', 'image', 'status', 'phone_number']
 
 
