19caf8bf6715bff73a77ce9f8d0473bc05fff9c1
darae07 <leedr0730@gmail.com>
2021-08-22 19:31:54 +0900

    user serializer contract filter 적용중

--- common/costume_serializers.py
+++ common/costume_serializers.py
@@ -1,10 +1,26 @@
+from group.models import Contract
 from group.serializer_contract import ContractSerializer
 from .serializers import UserSerializer
+from rest_framework import serializers
 
 
 class FullUserSerializer(UserSerializer):
-    contract = ContractSerializer(read_only=True, many=True)
+    # contract = serializers.SerializerMethodField('get_contract')
+    contract = ContractSerializer(many=True, read_only=True)
 
     class Meta(UserSerializer.Meta):
         fields = ['id', 'email', 'last_login',
                   'date_joined', 'nickname', 'image', 'status', 'phone_number', 'contract']
+
+    # def get_contract(self, instance):
+    #     company = None
+    #     request = self.context.get('request')
+    #     # print(self.context["request"].query_params.get("company", None))
+    #     # print(instance)
+    #     if request and hasattr(request, 'data'):
+    #         company = request.data.company
+    #         print(request.data)
+    #     if company:
+    #         contract = Contract.objects.filter(company=company)
+    #         serializer = ContractSerializer(contract=contract, many=True)
+    #         return serializer.data
