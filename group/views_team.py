f794ef918c937860a49e619a65e34ab9df65ee2a
darae07 <leedr0730@gmail.com>
2021-12-13 23:50:38 +0900

    팀 입장코드로 찾기, 입장 코드로 팀 찾 추가

--- group/views_team.py
+++ group/views_team.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.decorators import action, api_view, parser_classes, permission_classes
 from rest_framework.response import Response
 from .models import TeamMember, Team
@@ -16,6 +17,16 @@ class TeamViewSet(viewsets.ModelViewSet):
         queryset = self.queryset.filter(id__in=membership)
         return queryset
 
+    def partial_update(self, request, pk, *args, **kwargs):
+        data = request.data
+
+        team = Team.objects.get(pk=pk)
+        serializer = self.serializer_class(team, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save(**serializer.validated_data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     @action(detail=True)
     def members(self, request, pk=None):
         members = TeamMember.objects.filter(team=pk)
@@ -25,6 +36,16 @@ class TeamViewSet(viewsets.ModelViewSet):
             return self.get_paginated_response(serializer.data)
         serializer = TeamMemberSerializer(members, many=True)
         return Response(serializer.data)
+
+    @action(methods=['post'], detail=False)
+    def find(self, request):
+        code = request.data['code']
+        try:
+            team = Team.objects.get(join_code=code)
+            serializer = self.serializer_class(team, many=False)
+            return Response(data={**serializer.data, 'message': '인증코드로 팀을 찾았습니다.'})
+        except Team.DoesNotExist:
+            raise Http404
 
 
 @api_view(['post'])
