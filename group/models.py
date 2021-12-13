7a79acf63e5acd6487d46d47e5f2c809fbea7af6
darae07 <leedr0730@gmail.com>
2021-12-13 23:07:57 +0900

    팀멤버 이미지, 가입 코드 추가

--- group/models.py
+++ group/models.py
@@ -4,6 +4,8 @@ import uuid
 from django.db import models
 from stores.models import Store, Menu
 from django.contrib.gis.db.models import PointField
+import os
+import hashlib
 
 
 def unique_path(instance, filename):
@@ -15,14 +17,18 @@ def unique_path(instance, filename):
 def unique_path2(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join('group/team/member', filename)
+    return os.path.join('group/member', filename)
+
+
+def _create_id():
+    return uuid.uuid4().hex[:6].upper()
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100, unique=True)
     image = models.ImageField(upload_to=unique_path, default='')
     title = models.CharField(max_length=300, blank=True, null=True)
-    join_code = models.CharField(max_length=6, editable=False, default=uuid.uuid1())
+    join_code = models.CharField(max_length=8, editable=False, default=_create_id, unique=True)
 
 
 class TeamMember(models.Model):
@@ -32,7 +38,7 @@ class TeamMember(models.Model):
     member_name = models.CharField(max_length=100, blank=True, null=True)
     date_joined = models.DateField(auto_now_add=True, blank=True)
     image = models.ImageField(upload_to=unique_path2, default='')
-    title = models.CharField(max_length=300, blank=True, null=True)
+    title = models.CharField(max_length=6, blank=True, null=True)
 
     def __str__(self):
         return self.member_name
