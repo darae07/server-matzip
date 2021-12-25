5b3564b07191771aed09bb6a618136c14599f934
darae07 <leedr0730@gmail.com>
2021-12-26 00:47:33 +0900

    요청 핸들러 수정

--- matzip/handler.py
+++ matzip/handler.py
@@ -1,13 +1,17 @@
-def request_data_handler(data, required_fields, other_fields):
+def request_data_handler(data, required_fields=None, other_fields=None):
     formatted_data = data
-    for field in required_fields:
-        try:
-            value = formatted_data[field]
-        except KeyError:
-            raise Exception('필수 파라미터' + field + '가 누락되었습니다.')
+    if isinstance(required_fields, list):
+        for field in required_fields:
+            try:
+                value = formatted_data[field]
+            except KeyError:
+                raise Exception('필수 파라미터' + field + '가 누락되었습니다.')
 
-    for field in other_fields:
-        try:
-            value = formatted_data[field]
-        except KeyError:
-            formatted_data[field] = None
+    if isinstance(other_fields, list):
+        for field in other_fields:
+            try:
+                value = formatted_data[field]
+            except KeyError:
+                formatted_data[field] = None
+
+    return formatted_data
