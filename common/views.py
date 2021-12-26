9cd1e488f0cb3ccf3186899233a3c48cd807f215
darae07 <leedr0730@gmail.com>
2021-12-26 16:38:11 +0900

    카카오 로그인 뷰

--- common/views.py
+++ common/views.py
@@ -1,6 +1,11 @@
+import os
+
+import rest_framework
+from django.contrib import messages
 from django.db.models import Prefetch
+from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, request, status
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -13,6 +18,13 @@ from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.conf import settings
 from rest_framework.parsers import MultiPartParser
+from django.contrib.sites.models import Site
+
+
+current_site = Site.objects.get_current()
+KAKAO_CLIENT_ID = os.environ.get('KAKAO_ID')
+KAKAO_REDIRECT_URI = current_site.domain+'/common/kakao-callback/'
+RESPONSE_TYPE = 'code'
 
 
 class CommonUserViewSet(viewsets.ModelViewSet):
@@ -45,6 +57,19 @@ class CommonUserViewSet(viewsets.ModelViewSet):
         return Response(data={'id': pk}, status=status.HTTP_200_OK)
 
 
+def kakao_login(request):
+    try:
+        if request.user.is_authenticated:
+            raise Exception('이미 로그인한 유저입니다.')
+        return redirect(
+            f'https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_CLIENT_ID}&redirect_uri={KAKAO_REDIRECT_URI}'
+            f'&response_type={RESPONSE_TYPE}'
+        )
+    except Exception as e:
+        messages.error(request, e)
+        return redirect('core:home')
+
+
 class GoogleLogin(SocialLoginView):
     authentication_classes = []
     adapter_class = GoogleOAuth2Adapter
@@ -54,7 +79,7 @@ class GoogleLogin(SocialLoginView):
 
 @csrf_exempt
 def google_token():
-    if 'code' not in request.body.decode():
+    if 'code' not in rest_framework.request.body.decode():
         from rest_framework_simplejwt.settings import api_settings as jwt_settings
         from rest_framework_simplejwt.views import TokenRefreshView
 
