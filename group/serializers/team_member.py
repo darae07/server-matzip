bbc2454b4a8fc3bd79ba9ad74d0612ce26be4060
darae07 <leedr0730@gmail.com>
2022-01-16 18:08:38 +0900

    팀멤버 시리얼라이저 정리

--- group/serializers/team_member.py
+++ group/serializers/team_member.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-from common.serializers import UserSerializer
 from group.models_team import TeamMember
 
 
 class TeamMemberSerializer(serializers.ModelSerializer):
-    # user = UserSerializer(read_only=True, many=False)
 
     class Meta:
         model = TeamMember
