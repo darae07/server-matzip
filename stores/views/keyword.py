a7a15e63db5626ee509f36f440401d2ad8cf0ddc
darae07 <leedr0730@gmail.com>
2022-01-23 14:44:47 +0900

    keyword 생성 적용

--- stores/views/keyword.py
+++ stores/views/keyword.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
+from group.models_team import TeamMember
 from matzip.handler import request_data_handler
-from stores.models import Keyword
+from stores.models import Keyword, Category
 from stores.serializers.keword import KeywordSerializer, KeywordCreateSerializer
 
 
@@ -12,7 +13,7 @@ class KeywordViewSet(viewsets.ModelViewSet):
     serializer_class = KeywordSerializer
 
     def list(self, request, *args, **kwargs):
-        queryset = self.queryset.order_by('-created_at')
+        queryset = self.queryset.order_by('-hit_count')
         page = self.paginate_queryset(queryset)
         serializer = self.serializer_class(page, many=True)
         return self.get_paginated_response(serializer.data)
@@ -23,17 +24,12 @@ class KeywordViewSet(viewsets.ModelViewSet):
         return Response({**serializer.data, 'message': '키워드를 조회했습니다.'}, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
-        data = request_data_handler(request.data, ['name', 'team'], ['category'])
-        keyword = self.queryset.filter(naem=data['name'], team=data['team']).first()
-        if keyword:
-            keyword.hit_count += 1
-            keyword.save()
-            serializer = self.serializer_class(keyword)
-            return Response({**serializer.data, 'message': '키워드를 업데이트 했습니다.'})
-        else:
-            serializer = KeywordCreateSerializer(data=data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            serializer = self.serializer_class(serializer.instance)
-            return Response({**serializer.data, 'message': '키워드를 생성했습니다.'})
-        # keyword object create or update 생성 필요
+        user = request.user
+        team_member = TeamMember.objects.get_my_team_profile(user=user)
+        team = team_member.team
+        data = request_data_handler(request.data, ['name'], ['category'])
+
+        category = Category.objects.get(id=data['category'])
+        keyword = Keyword.objects.hit_keyword(name=data['name'], team=team, category=category)
+        serializer = self.serializer_class(keyword)
+        return Response({**serializer.data, 'message': '키워드를 등록했습니다.'})
