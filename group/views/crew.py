d308bc34fe0457fb9b2a3d96d9252afb6abb7837
darae07 <leedr0730@gmail.com>
2022-01-25 22:44:58 +0900

    내 크루 불러오기, 이미지 등록

--- group/views/crew.py
+++ group/views/crew.py
@@ -3,6 +3,7 @@ import datetime
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -44,7 +45,7 @@ class CrewViewSet(viewsets.ModelViewSet):
         if Crew.objects.filter(team=team_member.team.id, name=data['name']).first():
             return Response({'message': '크루명이 사용중입니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
         data['team'] = team_member.team.id
-        print(data)
+
         serializer = CrewSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -71,4 +72,31 @@ class CrewViewSet(viewsets.ModelViewSet):
         team_member = TeamMember.objects.get_my_team_profile(user=user)
         if not team_member:
             return Response({'message': '팀 권한이 없습니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-        queryset = self.queryset.filter(team=team_member.team.id, )
+        my_crew_membership_list = CrewMembership.objects.filter(team_member=team_member).values('crew')
+        queryset = self.queryset.filter(team=team_member.team.id, id__in=my_crew_membership_list)
+        my_crew = queryset.first()
+        if my_crew:
+            serializer = CrewDetailSerializer(my_crew)
+            return Response({'message': '나의 크루를 불러왔습니다.', **serializer.data}, status=status.HTTP_200_OK)
+        return Response({'message': '나의 크루를 찾을 수 없습니다.'}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['POST'], parser_classes=[MultiPartParser, FormParser])
+    def upload_image(self, request, pk=None):
+        user = request.user
+        team_member = TeamMember.objects.get_my_team_profile(user=user)
+        if not team_member:
+            return Response({'message': '팀 권한이 없습니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        try:
+            crew = Crew.objects.get(id=pk)
+            my_crew_membership_list = CrewMembership.objects.filter(team_member=team_member).values('crew')
+            if not my_crew_membership_list.filter(crew=pk).first():
+                return Response({'message': '권한이 없습니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        except Crew.DoesNotExist:
+            return Response({'message': '크루를 찾을 수 없습니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        if request.FILES:
+            request.data.image = request.FILES
+        serializer = CrewSerializer(crew, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(**serializer.validated_data)
+        serializer = CrewDetailSerializer(instance=serializer.instance)
+        return Response({'message': '크루 이미지를 등록했습니다.', **serializer.data}, status=status.HTTP_200_OK)
