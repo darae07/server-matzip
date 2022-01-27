eb7c06ef5836cc1f11a579975a558c444732ddee
darae07 <leedr0730@gmail.com>
2022-01-27 23:13:39 +0900

    안쓰는 url 정리

--- group/urls.py
+++ group/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from .views_invite import InviteViewSet
-from .views_party import TagViewSet
 from .views.team import TeamViewSet, TeamMemberViewSet
 from .views.party import PartyViewSet, MembershipViewSet
 from .views.crew import CrewViewSet, CrewMembershipViewSet, LunchViewSet, VoteViewSet
@@ -11,10 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'party', PartyViewSet)
 router.register(r'vote', VoteViewSet)
 router.register(r'party_membership', MembershipViewSet)
-router.register(r'invite', InviteViewSet)
 router.register(r'team', TeamViewSet)
 router.register(r'member', TeamMemberViewSet)
-router.register(r'tag', TagViewSet)
 router.register(r'crew', CrewViewSet)
 router.register(r'crew_membership', CrewMembershipViewSet)
 router.register(r'lunch', LunchViewSet)
