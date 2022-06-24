6ebfc3b0b43f75d36177bfe67c6cd9851b273f74
darae07 <leedr0730@gmail.com>
2022-06-24 16:50:44 +0900

    키워드에 맛있다 비율 리뷰 반영

--- stores/serializers/keword.py
+++ stores/serializers/keword.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.db.models import Avg
+
 from stores.models import Keyword
 from stores.serializer import CategorySerializer
 
@@ -14,9 +15,18 @@ class KeywordCreateSerializer(serializers.ModelSerializer):
 class KeywordSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True, many=False)
     score = serializers.SerializerMethodField('get_score')
+    eat_count = serializers.SerializerMethodField('get_eat_count')
+    good_count = serializers.SerializerMethodField('get_good_count')
 
     def get_score(self, instance):
         return instance.reviews.all().aggregate(Avg('score'))['score__avg']
+
+    def get_eat_count(self, instance):
+        return instance.reviews.all().count()
+
+    def get_good_count(self, instance):
+        reviews = instance.reviews.all()
+        return reviews.filter(score__gte=4).count()
 
     class Meta:
         model = Keyword
